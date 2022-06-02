b37676645d6a2af6694ffc3e0653c570eb87f8c0
root <Dame>
2022-06-02 12:33:14 -0700

    Correcterd 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 
-int main (void)
+/**main: prints the specified string to standard output
+ * Rerurn: returns 0*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
