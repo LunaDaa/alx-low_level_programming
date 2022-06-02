1fbe721b9c02ecfab6d426983f511ac32f41e8ae
root <Dame>
2022-06-02 12:46:26 -0700

    Added file 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/*
+ * main- prints a string to the standard output
+ * Return: returns 0
+ */ 
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
