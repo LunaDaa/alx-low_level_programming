46ac7f39349776f226781bee6e7a9705d6285624
root <Dame>
2022-06-02 13:46:37 -0700

    edited 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 /**
  * main- prints to standard error
- * Return: returns 0
+ * Return: returns 1
  **/
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" ");
 	fprintf(stderr, "- Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
