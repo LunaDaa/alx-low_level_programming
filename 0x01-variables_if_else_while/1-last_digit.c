922ebc7ac01884b86bf811ebe4f842a34e577d22
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-06 14:41:55 -0800

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	last_digit = n % 10
+	last_digit = n % 10;
 	if (last_digit > 5)
 		strcpy(m, "greater than 5");
 	else if (last_digit == 0)
