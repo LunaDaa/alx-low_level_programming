1b65eda08538f3eb593f27ed70e0facc6d2b4340
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-06 13:31:10 -0800

    Create 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main- The functions create a rundom number and
+*prints its last digit and weather it is greater than 5, zero or less than 6
+*Return: Returns zero
+**/
+int main(void)
+{
+	int n;
+  String m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	String string_n = string(n);
+  String string_last_digit = string_n[-1];
+  int last_digit = int(string_last_digit);
+  if (last_digit > 5)
+    m = "greater than 5";
+  else if (last_digit == 0)
+    m = "0";
+  else
+    m = "less than 6 and not 0";
+  printf("Last digit of %d is %d and is %s", n, last_digit, m);
+	return (0);
+}
