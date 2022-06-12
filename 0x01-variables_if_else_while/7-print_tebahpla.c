81404b5e9339d4f6703c248a42cca3689a55b00c
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-12 10:14:47 -0800

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+*main- prints small letter alphabets in reverse order
+*Return: Returns 0
+**/
+int main(void)
+{
+char i;
+
+for (i = 'z'; i > = 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return(0);
+}
