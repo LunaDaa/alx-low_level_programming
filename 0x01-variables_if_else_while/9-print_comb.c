544ace8bd9883f53eb78098a168d354794f8de48
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-12 10:39:29 -0800

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
 *main- prints all possible combination of single digit number
-*Return returns 0
+*Return: returns 0
 **/
 int main(void)
 {
@@ -9,7 +9,7 @@ int i;
 
 for (i = 0; i <=9; i++)
 {
-putchar(i);
+putchar(i + '0');
 putchar(',');
 putchar(' ');
 }
