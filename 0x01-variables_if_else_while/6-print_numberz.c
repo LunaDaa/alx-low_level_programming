33cee54655c0d3f63b5906a0c8b1dc57c2ca2c25
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-12 09:55:09 -0800

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+*main- prints numbers from 0 to 9
+*Return: Returns 0
+**/
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+putchar((char)i);
+}
+putchar('\n');
+return (0);
+}
