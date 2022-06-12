eee86bdaed5c73ce7f548dab24d318450f737f6e
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-12 12:08:55 -0800

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+*main- prints ...
+*Return: Returns 0
+**/
+int main(void)
+{
+int i;
+int j;
+
+for (i = 0; i < 100; i++)
+{
+for (j = i + 1; j < 100; j++)
+{
+if (i < 10)
+putchar(0 + '0')
+putchar(i + '0');
+putchar(' ');
+if (i < 10)
+putchar(0 + '0')
+putchar(j + '0');
+
+if (i == 98 && j == 99)
+break;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
