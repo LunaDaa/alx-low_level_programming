7af1de1dba3810efe72281554c4d3743b090b97b
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-12 08:21:54 -0800

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
-
+/**
+*main- prints all small letter alphabets followed by
+*      capital letters alphabet
+*Returns- Always returns 0       
+**/
 int main()
 {
+  char i;
+  char j;
   
+  for (i = a; i <= z; i++)
+  {
+    putchar (i);
+  }
+  for (j = A; j <= Z; i++)
+  {
+    putchar (j);
+  }
+  putchar ("\n");
+  return (0)
 }
