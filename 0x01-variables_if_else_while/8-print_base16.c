cc9c2182f450fb21eafca4a7a626e276b17013ac
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-12 10:28:02 -0800

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+*main- prints all numbers with digit 16
+*Return: Returns 0
+**/
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'f'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
