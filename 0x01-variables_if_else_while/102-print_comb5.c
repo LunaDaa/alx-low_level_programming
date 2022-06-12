aee8ac3590a543be569c5187f7d87c5989f35357
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-12 12:09:34 -0800

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@ for (i = 0; i < 100; i++)
 for (j = i + 1; j < 100; j++)
 {
 if (i < 10)
-putchar(0 + '0')
+putchar(0 + '0');
 putchar(i + '0');
 putchar(' ');
 if (i < 10)
-putchar(0 + '0')
+putchar(0 + '0');
 putchar(j + '0');
 
 if (i == 98 && j == 99)
