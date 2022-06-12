4d046bce6928389329d9aee13bf0b4af61c68505
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-12 11:01:37 -0800

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,9 +12,9 @@ int main(void)
 int i;
 int j;
 
-for (i = 0; i <10; i++)
+for (i = 0; i < 10; i++)
 {
-for (j = i+1; j < 10; j++)
+for (j = i + 1; j < 10; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
