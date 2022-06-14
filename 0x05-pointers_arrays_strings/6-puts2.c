f48b3d3e700190d9d8be66a677eb257c8ee77cca
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-14 09:36:55 -0800

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*puts2- prints every other character of a string
+*return: Nothing
+*@str: a string variable
+**/
+void puts2(char *str)
+{
+int i;
+
+for (i = 0; i != '\0'; i+2)
+{
+putchar (str[i]);
+}
+putchar ('\n');
+}
