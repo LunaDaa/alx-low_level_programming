e6e2c85826677832a829d037d5c75a4ee1caa665
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-14 09:03:46 -0800

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+*print_rev-prints string in reverse form
+*Return: nothing
+*@s: a string variable
+**/
 void print_rev(char *s)
 {
 int len;
@@ -10,9 +16,9 @@ for (i = 0; s[i] != '\0'; i++)
 {
 len++;
 }
-for (i = len-2; i >= 0; i--)
+for (i = len-1; i >= 0; i--)
 {
 putchar(s[i]);
 }
-
+putchar('\n');
 }
