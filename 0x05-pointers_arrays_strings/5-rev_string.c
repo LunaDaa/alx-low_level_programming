d6c2326a51087546f8f50f7e5e6ec86b61663738
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-14 09:10:14 -0800

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_rev-prints string in reverse form
+*Return: nothing
+*@s: a string variable
+**/
+void rev_string(char *s)
+{
+int len;
+int i;
+int j;
+char *temp;
+
+len = 0;
+j = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+len++;
+}
+for (i = len - 1; i >= 0; i--)
+{
+temp[j] =s[i];
+j++;
+}
+s = temp;
+}
