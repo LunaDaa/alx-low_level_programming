46d3aa6772e70fb22dc8b3e04f938f4ba47a1541
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-14 07:33:22 -0800

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+* _puts- prints string
+*Return: nothing
+*@str: a variable that holds a string
 void _puts(char *str)
 {
 int i;
@@ -9,4 +12,5 @@ for (i = 0; str[i] != '\0'; i++)
 {
 putchar (str[i]);
 }
+putchar ('\n');
 }
