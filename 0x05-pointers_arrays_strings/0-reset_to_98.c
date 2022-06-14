d942d6348aa0f416ef72aa52ed7be5978bf19e5f
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-14 04:02:52 -0800

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,6 @@
 #include "main.h"
 
 /**
-reset_to_98-changes the value of the variable it points to 98
-Return: Nothing
-**/
-void reset_to_98(int *n)
-{
-*n = 98;
-}
-
-/**
 main - 
 Return: Returns 0
 **/
@@ -22,4 +13,14 @@ m = 87;
 p = &m;
 reset_to_98(p);
 return (0);
+}
+
+
+/**
+reset_to_98-changes the value of the variable it points to 98
+Return: Nothing
+**/
+void reset_to_98(int *n)
+{
+*n = 98;
 }
