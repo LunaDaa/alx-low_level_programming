bf6fa49582d8706021f145fac2c1b1fa239cb724
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-14 04:58:51 -0800

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-void reset_to_98(int *n);
+/**
+reset_to_98-changes the value of the variable it points to 98
+Return: Nothing
+**/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
 
 #endif
