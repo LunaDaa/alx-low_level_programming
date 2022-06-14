51f9bcc70bf84e348eda15ec81cdfd27009ed06d
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-14 05:36:48 -0800

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-swap_int - swaps the value the pointers a nd b holds
-Return: Nothing
-@temp: a temporary value to hold one of the values athe pointers old to
+*swap_int - swaps the value the pointers a nd b holds
+*Return: Nothing
+*@temp: a temporary value to hold one of the values athe pointers old to
 **/
 void swap_int(int *a, int *b)
 {
