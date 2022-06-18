65f2a07e9c6abbd82612baae61da4101843fa8ed
LunaDaa <104769557+LunaDaa@users.noreply.github.com>
2022-06-17 23:10:29 -0800

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@
  *       used in the 0x02-functions_nested_loops directory.
  */
 
-int _putchar(char c);
+int _putchar(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
